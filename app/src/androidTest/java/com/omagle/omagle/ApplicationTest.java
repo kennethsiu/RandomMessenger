49f3a5dbdc16f44d794023c07a7e4af0856ee224
Mohammed Omar <moham@BK201>
2016-10-22 19:09:55 -0700

    Commit test

--- app/src/androidTest/java/com/omagle/omagle/ApplicationTest.java
+++ app/src/androidTest/java/com/omagle/omagle/ApplicationTest.java
@@ -3,7 +3,7 @@ package com.omagle.omagle;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
+/** This is a comment
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {

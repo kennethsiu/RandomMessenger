74b9495da02ce0be441b345ff41c4dbbda1e91b1
James Doohyun Kwon <doohyunkwon@gmail.com>
2016-10-24 00:56:46 -0700

    DONOT MERGE THIS OR YOU WILL BREAK CODE

---
+++ app/src/main/java/com/omagle/omagle/StartChat.java
@@ -0,0 +1,13 @@
+package com.omagle.omagle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_chat);
+    }
+}

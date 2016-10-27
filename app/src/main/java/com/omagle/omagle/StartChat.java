152df8cfaa774ee0c826ff72972a64a987544d91
James Doohyun Kwon <doohyunkwon@gmail.com>
2016-10-26 21:33:57 -0700

    working build hallelujah

--- app/src/main/java/com/omagle/omagle/StartChat.java
+++ app/src/main/java/com/omagle/omagle/StartChat.java
@@ -1,7 +1,10 @@
 package com.omagle.omagle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartChat extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class StartChat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_chat);
+
+        final Button signOutButton = (Button) findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut(v);
+            }
+        });
+    }
+    private void signOut(View view) {
+        Intent signOutIntent = new Intent(this, LoginActivity.class);
+        startActivity(signOutIntent);
     }
 }

270f49b0ed3982be3327901cedb128d994b70b08
jdkwon <doohyunkwon@gmail.com>
2016-11-20 22:22:27 -0800

    Left an extra paranthesis that was crashing the app. I'm working on getting an error fixed so I can build the apk.

--- app/src/main/java/com/omagle/omagle/SignUp.java
+++ app/src/main/java/com/omagle/omagle/SignUp.java
@@ -35,7 +35,7 @@ public class SignUp extends AppCompatActivity {
     private EditText passw;
     private EditText confPass;
 
-    private String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@ucsd.edu)";
+    private String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@ucsd.edu";
 
     final String auth_failed = "Failed to create account";
 
